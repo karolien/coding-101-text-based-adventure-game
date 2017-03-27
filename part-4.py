49ee1136cd3c7b6b8c07f18ce3bdfc30035613e0
Karolien <kkoo939@aucklanduni.ac.nz>
2017-03-27 17:03:23 +1300

    Update part-4.py

--- part-4.py
+++ part-4.py
@@ -2,5 +2,5 @@ print("Welcome to Choose Your Own Adventure")
 name = input("Please enter a character name: ") #introduce variables
 print("Hi, " + name + " thank you for playing.")
 print("Please enter your choice for race: \n1. Elf\n2. Goblin\n3. Dwarf\n4. Human")
-race = ("Enter your choice: ")
+race = input("Enter your choice: ")
 print("You chose " + race)
